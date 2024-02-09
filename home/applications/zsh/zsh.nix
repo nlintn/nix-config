8f85eecbb1a32bfc3ca22c4e4541a5ef97a7bebf
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2024-02-09 01:47:49 +0100

    zsh config restructuring

--- home/applications/zsh/zsh.nix
+++ home/applications/zsh/zsh.nix
@@ -1,10 +1,6 @@
 { inputs, userSettings, ... }:
 
-let 
-  custom_dir = ".oh-my-custom";
-in {
-  home.file.${custom_dir + "/themes/meoww.zsh-theme"}.source = ./meoww.zsh-theme;
-
+{
   programs.zsh = {
     enable = true;
     plugins = [
@@ -18,13 +14,17 @@ in {
     syntaxHighlighting.enable = true;
     oh-my-zsh = {
       enable = true;
-      custom = "$HOME/" + custom_dir;
-      plugins = [ "git" "sudo" "colored-man-pages" "copyfile" "copypath" "per-directory-history" "themes"];
+      custom = builtins.toString ./custom;
+      plugins = [ "git" "sudo" "colored-man-pages" "per-directory-history" "themes"];
       theme = "meoww";
     };
     shellAliases = {
       c = "clear";
+      dotdir = "builtin cd $DOT_DIR";
+      hms = "home-manager switch --flake $DOT_DIR";
+      nrs = "sudo nixos-rebuild switch --flake $DOT_DIR";
     };
+    # cdpath = [ "HOME/dotfiles" "$HOME/TUM" ];
     # Opam env
     # initExtra = ''
     #   [[ ! -r /home/nico/.opam/opam-init/init.zsh ]] || source /home/nico/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null

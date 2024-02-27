4f1ec2cb6e8a5305d3f78534e241f6f36c7a5f3e
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2024-02-27 19:47:21 +0100

    more firefox settings

--- home/applications/firefox.nix
+++ home/applications/firefox.nix
@@ -5,10 +5,16 @@
     enable = true;
     package = pkgs.firefox-wayland; 
     profiles."nico" = {
+      settings = {
+        "browser.fullscreen.autohide" = false;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      };
       userChrome = ''
         :root[inFullscreen] #PersonalToolbar {
           visibility: visible !important;
         }
+        .titlebar-buttonbox-container{ display:none } /* Remove close button*/ 
+        .titlebar-spacer[type="post-tabs"]{ display:none }
       '';
     };
   };

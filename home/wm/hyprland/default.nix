74551f42d82c29937d046be1f153882c680744d9
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2024-04-12 18:26:09 +0200

    removed hyprland substituter

--- home/wm/hyprland/default.nix
+++ home/wm/hyprland/default.nix
@@ -69,9 +69,4 @@ in {
     save_dir=${config.home.homeDirectory}/Pictures/Screenshots
     save_filename_format=screen-%Y%m%d-%H%M%S.png
   '';
-
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
 }

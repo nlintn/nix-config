e9e0e01d3531b037419325dc625e4f0542a2b8ef
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-08-08 02:17:52 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/13461dec40bf03d9196ff79d1abe48408268cc35?narHash=sha256-V0iiDcYvNeMOP2FyfgC4H8Esx%2BJodXEl80lD4hFD4SI%3D' (2025-08-06)
      → 'github:nix-community/home-manager/5de16c704b0fc8f519b2c19ed3f683a9e68f3884?narHash=sha256-bo6aSfDS/GGfM/6LXCKLH/246fDSKjFnBsaRMNE%2BWmc%3D' (2025-08-07)
    • Updated input 'nixpkgs':
        'path:/nix/store/l98x43ylm65fckcml3fmcsxlyf748qkc-source?lastModified=0&narHash=sha256-erbiH2agUTD0Z30xcVSFcDHzkRvkRXOQ3lb887bcVrs%3D' (1970-01-01)
      → 'github:nixos/nixpkgs/c2ae88e026f9525daf89587f3cbee584b92b6134?narHash=sha256-erbiH2agUTD0Z30xcVSFcDHzkRvkRXOQ3lb887bcVrs%3D' (2025-08-06)

--- hosts/systems/meoww/displaymanager.nix
+++ hosts/systems/meoww/displaymanager.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       default_session = let session_dir = "${config.services.displayManager.sessionData.desktops}/share"; in {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format \"%d. %b %Y %H:%M:%S\" --asterisks --user-menu --remember
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --time-format \"%d. %b %Y %H:%M:%S\" --asterisks --user-menu --remember
           --remember-user-session --sessions ${session_dir}/wayland-sessions --xsessions ${session_dir}/xsessions";
       };
     };

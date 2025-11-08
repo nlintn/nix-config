d53589a267480e5a6a058fb6e383b57a5b77c459
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-11-08 03:09:56 +0100

    further tmux config + sesh config

--- home/users/nico/applications/tmux.nix
+++ home/users/nico/applications/tmux.nix
@@ -1,36 +1,50 @@
 { config, lib, pkgs, ... }:
 
 let
-  seshBin = lib.getExe config.programs.sesh.package;
-  seshFzf = ''
-    ${seshBin} connect "$(
-      ${seshBin} list --icons | fzf --tmux 80%,70% \
+  seshFzf = let
+    fd = lib.getExe pkgs.fd;
+    fzf = lib.getExe config.programs.fzf.package;
+    rg = lib.getExe pkgs.ripgrep;
+    sesh = lib.getExe config.programs.sesh.package;
+    tmux = lib.getExe config.programs.tmux.package;
+  in pkgs.writeShellScript "sesh-fzf" ''
+    ${sesh} connect "$(
+      ${sesh} list --icons | (${rg} -v '_popup_.*_' || true) | ${fzf} --tmux center,80%,70% \
         --no-sort --ansi --border-label ' sesh ' --prompt '󱐋  ' \
         --header '  󰘴a all 󰘴t tmux 󰘴g configs 󰘴x zoxide 󰘴d kill 󰘴f find' \
         --bind 'tab:down,btab:up' \
-        --bind 'ctrl-a:change-prompt(󱐋  )+reload(${seshBin} list --icons)' \
-        --bind 'ctrl-t:change-prompt(  )+reload(${seshBin} list -t --icons)' \
-        --bind 'ctrl-g:change-prompt(  )+reload(${seshBin} list -c --icons)' \
-        --bind 'ctrl-x:change-prompt(󰉋  )+reload(${seshBin} list -z --icons)' \
-        --bind 'ctrl-f:change-prompt(  )+reload(${lib.getExe pkgs.fd} -IL -t d . ~)' \
-        --bind 'ctrl-d:execute(${lib.getExe config.programs.tmux.package} kill-session -t {2..})+change-prompt(󱐋  )+reload(${seshBin} list --icons)' \
+        --bind 'ctrl-a:change-prompt(󱐋  )+reload(${sesh} list --icons | ${rg} -v "_popup_.*_" || true)' \
+        --bind 'ctrl-t:change-prompt(  )+reload(${sesh} list -t --icons | ${rg} -v "_popup_.*_" || true)' \
+        --bind 'ctrl-g:change-prompt(  )+reload(${sesh} list -c --icons)' \
+        --bind 'ctrl-x:change-prompt(󰉋  )+reload(${sesh} list -z --icons)' \
+        --bind 'ctrl-f:change-prompt(  )+reload(${fd} -IL -t d . ~)' \
+        --bind 'ctrl-d:execute(${tmux} kill-session -t {2..})+change-prompt(󱐋  )+reload(${sesh} list --icons)' \
         --preview-window 'right:60%' \
-        --preview '${seshBin} preview {}'
+        --preview '${sesh} preview {}'
     )"'';
 in {
   programs.tmux = {
     enable = true;
-    mouse = true;
-    clock24 = true;
-    newSession = true;
-    prefix = "C-a";
-    keyMode = "vi";
-    escapeTime = 0;
-    focusEvents = true;
-
     extraConfig = ''
+      unbind C-b
+      set -g prefix C-a
+      bind -n C-a send-prefix
+      set -g status-keys vi
+      set -g mode-keys vi
+      set -g mouse on
+
+      set -g focus-events on
+      set -g aggressive-resize off
+      set -g clock-mode-style 24
+      set -g escape-time 0
+      set -g history-limit 2000
+
       set -g set-titles on
       set -g set-titles-string '#{pane_title}'
+
+      set -g base-index 1
+      set -g pane-base-index 1
+      set -g renumber-windows on
 
       set -g status-position top
       set -g status-justify absolute-centre
@@ -39,6 +53,7 @@ in {
       set -g status-left ""
       set -g status-right " #S"
 
+      set -g default-terminal "tmux-256color"
       set -g allow-passthrough all
       set -ga update-environment TERM
       set -ga update-environment TERM_PROGRAM
@@ -53,13 +68,35 @@ in {
       bind ü copy-mode
       bind x kill-pane # skip "kill-pane? (y/n)" prompt
 
-      bind a run-shell "${lib.escape [ "\"" ] seshFzf }"
+      bind a run-shell "${seshFzf}"
 
       bind g display-popup -b rounded -E -xC -yC -w 90% -h 90% -d "#{pane_current_path}" ${lib.getExe config.programs.lazygit.package}
 
       # vim like selection keys
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+      bind -n C-Space display-popup -b rounded -xC -yC -w 65% -h 65% -E ${let
+        tmux = lib.getExe config.programs.tmux.package;
+      in pkgs.writeShellScript "tmux-popup" ''
+        session="_popup_$(${tmux} display -p '#S')_"
+
+        if ! ${tmux} has -t "$session" 2> /dev/null; then
+          session_id="$(${tmux} new-session -dP -s "$session" -F '#{session_id}')"
+          ${tmux} set-option -s -t "$session_id" key-table _popup
+          ${tmux} set-option -s -t "$session_id" status off
+          ${tmux} set-option -s -t "$session_id" prefix None
+          ${tmux} set-environment -t "$session_id" FZF_TMUX 1
+          session="$session_id"
+        fi
+
+        builtin exec ${tmux} attach -t "$session" > /dev/null
+      ''}
+      bind -T _popup C-Space detach
+      bind -T _popup C-[ copy-mode
+
+      bind w choose-tree -Z -f '#{?#{m:_popup_*_,#{session_name}},0,1}'
+      bind s choose-tree -Zs -f '#{?#{m:_popup_*_,#{session_name}},0,1}'
     '';
   };
 
@@ -69,8 +106,19 @@ in {
     enableTmuxIntegration = false;
     fzfPackage = config.programs.fzf.package;
     zoxidePackage = config.programs.zoxide.package;
+    settings = {
+      default_session = {
+        preview_command = "${lib.getExe pkgs.eza} --color=always --follow-symlinks --tree {}";
+      };
+      session = [
+        {
+          name = "nix-config";
+          path = config.home.sessionVariables.NIX_CONFIG_DIR;
+          startup_command = "${lib.getExe config.vars.nvimPackage} -c ':Telescope find_files'";
+        }
+      ];
+    };
   };
 
-  programs.fzf.tmux.enableShellIntegration = true;
-  home.shellAliases.s = seshFzf;
+  home.shellAliases.s = builtins.toString seshFzf;
 }

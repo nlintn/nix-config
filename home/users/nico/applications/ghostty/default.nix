2b80a0de1bb303f8f83f81acea6f23b09fa0213c
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-11-08 03:09:34 +0100

    ghostty keybinds

--- home/users/nico/applications/ghostty/default.nix
+++ home/users/nico/applications/ghostty/default.nix
@@ -4,6 +4,7 @@
   programs.ghostty = {
     enable = true;
     themes.base16 = import ./base16.nix args;
+    clearDefaultKeybinds = true;
     settings = {
       auto-update = "off";
       app-notifications = false;
@@ -22,6 +23,28 @@
       window-inherit-font-size = true;
 
       shell-integration-features = "ssh-env, sudo, title, no-cursor";
+
+      keybind = [
+        "ctrl+shift+a=select_all"
+        "ctrl+shift+c=copy_to_clipboard"
+        "ctrl+shift+i=inspector:toggle"
+        "ctrl+shift+n=new_window"
+        "ctrl+shift+p=toggle_command_palette"
+        "ctrl+shift+page_down=jump_to_prompt:1"
+        "ctrl+shift+page_up=jump_to_prompt:-1"
+        "ctrl+shift+q=quit"
+        "ctrl+shift+r=reload_config"
+        "ctrl+shift+v=paste_from_clipboard"
+
+        "ctrl+shift+t=new_tab"
+        "ctrl+shift+w=close_tab:this"
+        "ctrl+shift+arrow_left=previous_tab"
+        "ctrl+shift+arrow_right=next_tab"
+
+        "ctrl+shift++=increase_font_size:1"
+        "ctrl+shift+-=decrease_font_size:1"
+        "ctrl+shift+0=reset_font_size"
+      ];
     };
   };
 }

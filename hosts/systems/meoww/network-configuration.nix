1463e37fa4c619a9246da98f6999b552e8634d11
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2026-01-05 03:15:37 +0100

    rewrite dnscrypt-proxy config

--- hosts/systems/meoww/network-configuration.nix
+++ hosts/systems/meoww/network-configuration.nix
@@ -24,8 +24,11 @@
 
   services.dnscrypt-proxy = {
     enable = true;
+    upstreamDefaults = false;
     settings = {
-      # query_log.file = "/var/log/dnscrypt-proxy/query.log";
+      listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
+      max_clients = 250;
+
       ipv4_servers = true;
       ipv6_servers = true;
       dnscrypt_servers = true;
@@ -34,20 +37,102 @@
       require_dnssec = true;
       require_nolog = true;
       require_nofilter = true;
-      # skip_incompatible = false;
+      disabled_server_names = [];
 
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-        ];
-        cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      force_tcp = false;
+      http3 = true;
+      http3_probe = false;
+
+      timeout = 5000;
+      keepalive = 30;
+
+      use_syslog = true;
+
+      bootstrap_resolvers = [ "9.9.9.11:53" "149.112.112.11:53" "[2620:fe::11]:53" "[2620:fe::fe:11]:53" ];
+      ignore_system_dns = true;
+      netprobe_timeout = 60;
+      netprobe_address = "9.9.9.9:53";
+
+      block_ipv6 = false;
+      block_unqualified = true;
+      block_undelegated = true;
+      reject_ttl = 10;
+
+      forwarding_rules = pkgs.writeText "dnscrypt-forwarding-rules" ''
+        fritz.box             $DHCP
+
+        iceportal.de          $DHCP
+        login.wifionice.de    $DHCP
+      '';
+
+      cloaking_rules = "/etc/dnscrypt-cloaking-rules";
+      cloak_ptr = false;
+
+      cache = true;
+      cache_size = 4096;
+      cache_min_ttl = 2400;
+      cache_max_ttl = 86400;
+      cache_neg_min_ttl = 60;
+      cache_neg_max_ttl = 600;
+
+      sources = {
+        public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+          refresh_delay = 73;
+          prefix = "";
+        };
+        quad9-resolvers = {
+          urls = [
+            "https://quad9.net/dnscrypt/quad9-resolvers.md"
+            "https://raw.githubusercontent.com/Quad9DNS/dnscrypt-settings/main/dnscrypt/quad9-resolvers.md"
+          ];
+          cache_file = "/var/cache/dnscrypt-proxy/quad9-resolvers.md";
+          minisign_key = "RWQBphd2+f6eiAqBsvDZEBXBGHQBJfeG6G+wJPPKxCZMoEQYpmoysKUN";
+          refresh_delay = 73;
+          prefix = "quad9-";
+        };
+      };
+
+      broken_implementations.fragments_blocked = [
+        "cisco"
+        "cisco-ipv6"
+        "cisco-familyshield"
+        "cisco-familyshield-ipv6"
+        "cisco-sandbox"
+        "cleanbrowsing-adult"
+        "cleanbrowsing-adult-ipv6"
+        "cleanbrowsing-family"
+        "cleanbrowsing-family-ipv6"
+        "cleanbrowsing-security"
+        "cleanbrowsing-security-ipv6"
+      ];
+
+      anonymized_dns.skip_incompatible = false;
+
+      ip_encryption.algorithm = "none";
+
+      monitoring_ui = {
+        enabled = true;
+        listen_address = "127.0.0.1:5335";
+        username = "";
+        password = "";
+        enable_query_log = true;
+        privacy_level = 0;
       };
     };
   };
   environment.shellAliases."dns-restart" = "${lib.getExe' config.systemd.package "systemctl"} restart dnscrypt-proxy2.service";
   environment.shellAliases."dns-stop" = "${lib.getExe' config.systemd.package "systemctl"} stop dnscrypt-proxy2.service";
+
+  environment.etc."dnscrypt-cloaking-rules" = {
+    text = "";
+    mode = "0644";
+  };
 
   systemd.services.NetworkManager-wait-online.enable = false;
 }

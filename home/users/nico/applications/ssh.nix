8cb0beadbb72ed2743dcfd1033cadddae71f97bd
Nico Lintner <git@nlintn.de>
2026-04-30 02:17:07 +0200

    fix(home): use wrapper for ssh-askpass to set $DISPLAY

--- home/users/nico/applications/ssh.nix
+++ home/users/nico/applications/ssh.nix
@@ -1,9 +1,20 @@
 {
+  config,
   lib,
   pkgs,
   ...
 }:
 
+let
+  grep = lib.getExe pkgs.gnugrep;
+  systemctl = config.systemd.user.systemctlPath;
+
+  askpass = "${pkgs.openssh-askpass}/libexec/gtk-ssh-askpass";
+  askpassWrapper = pkgs.writeShellScript "ssh-askpass-wrapper" ''
+    eval export $(${systemctl} --user show-environment | ${grep} -E '^(DISPLAY|WAYLAND_DISPLAY|XAUTHORITY)=')
+    exec ${askpass} "$@"
+  '';
+in
 {
   programs.ssh = {
     enable = true;
@@ -19,5 +30,9 @@
   };
 
   services.ssh-agent.enable = true;
-  home.sessionVariables.SSH_ASKPASS = "${pkgs.openssh-askpass}/libexec/gtk-ssh-askpass";
+  systemd.user.services.ssh-agent.Service.Environment = [
+    "DISPLAY=fake"
+    "SSH_ASKPASS=${askpassWrapper}"
+  ];
+  home.sessionVariables.SSH_ASKPASS = askpass;
 }

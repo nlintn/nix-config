c9d57f5a4ed745e338a4efcb48347e91244d9429
Nico Lintner <git@nlintn.de>
2026-06-26 11:01:07 +0200

    refactor(zsh): clean up and format

--- home/users/nico/shell/zsh/init.zsh
+++ home/users/nico/shell/zsh/init.zsh
@@ -6,7 +6,7 @@ zstyle ':completion:*' group-name ''
 zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*:warnings' format "${fg[red]}No matches for:${reset_color} %d"
 
-zstyle ':fzf-tab:complete:*:*' fzf-preview 'builtin echo -e "$group\n"; (builtin test -e "$realpath" && ($_cmd_bat --paging=never --color=always "${realpath}" || $_cmd_eza --color=always --follow-symlinks --tree "$realpath") || <<< "$desc") 2> /dev/null | $_cmd_head -98'
+zstyle ':fzf-tab:complete:*:*' fzf-preview 'builtin echo -e "${group}\n"; (builtin test -e "${realpath}" && ("${_cmd_bat}" --paging=never --color=always "${realpath}" || "${_cmd_eza}" --color=always --follow-symlinks --tree "${realpath}") || <<< "${desc}") 2> /dev/null | "${_cmd_head}" -98'
 zstyle ':fzf-tab:*' use-fzf-default-opts yes
 zstyle ':fzf-tab:*' fzf-flags --border-label ' command ' --exact --height '~-2' --preview-window 'right:40%,~2'
 zstyle ':fzf-tab:*' switch-group '<' '>'
@@ -23,56 +23,56 @@ bindkey "^[[3;6~" delete-word
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
-bindkey "$terminfo[kcuu1]" history-beginning-search-backward-end
-bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
+bindkey "${terminfo[kcuu1]}" history-beginning-search-backward-end
+bindkey "${terminfo[kcud1]}" history-beginning-search-forward-end
 
 KEYTIMEOUT=1
-bindkey -v "$terminfo[kbs]" backward-delete-char
+bindkey -v "${terminfo[kbs]}" backward-delete-char
 
 autoload edit-command-line &&
-zle -N edit-command-line &&
-bindkey -a "^V" edit-command-line
+  zle -N edit-command-line &&
+  bindkey -a "^V" edit-command-line
 
 copy() {
   if [ "${#}" = "0" ]; then
-    local formatted="$($_cmd_base64 -w 0 /dev/stdin)"
+    local formatted="$("${_cmd_base64}" -w 0 /dev/stdin)"
   else
-    local arg="$@"
-    local formatted="$(builtin printf %s $arg | $_cmd_base64 -w 0)"
+    local arg="${@}"
+    local formatted="$(builtin printf %s ${arg} | "${_cmd_base64}" -w 0)"
   fi
-  builtin printf "\e]52;c;%s${terminfo[bel]}" "$formatted"
+  builtin printf "\e]52;c;%s${terminfo[bel]}" "${formatted}"
 }
 
 paste() {
   if which wl-paste &>/dev/null; then
     wl-paste
   else
-    builtin printf "\e]52;c;?${terminfo[bel]}" > /dev/tty
+    builtin printf "\e]52;c;?${terminfo[bel]}" >/dev/tty
 
     local -A result
-    read -d "\\" result < /dev/tty
+    builtin read -d "\\" result </dev/tty
     if [ "${result:0:7}" != "$(printf "\e]52;c;")" ]; then
       builtin echo "paste failed" >&2
       return 1
     fi
 
     if [ "$1" != "widget" ]; then
-      builtin printf "${terminfo[dl1]}" > /dev/tty
+      builtin printf "${terminfo[dl1]}" >/dev/tty
     fi
 
-    $_cmd_base64 -dw 0 <<< "${result:7:-1}"
+    "${_cmd_base64}" -dw 0 <<<"${result:7:-1}"
   fi
 }
 
 wrap_clipboard_widgets_copy() {
   local widget
   local wrapped_name
-  for widget in "$@"; do
+  for widget in "${@}"; do
     wrapped_name="_zsh-vi-copy-${widget}"
     eval "
       ${wrapped_name}() {
         zle .${widget}
-        copy \"\$CUTBUFFER\"
+        copy \"\${CUTBUFFER}\"
       }
     "
     zle -N "${widget}" "${wrapped_name}"
@@ -81,11 +81,11 @@ wrap_clipboard_widgets_copy() {
 wrap_clipboard_widgets_paste() {
   local widget
   local wrapped_name
-  for widget in "$@"; do
+  for widget in "${@}"; do
     wrapped_name="_zsh-vi-paste-${widget}"
     eval "
       ${wrapped_name}() {
-        CUTBUFFER=\"\$(paste widget 2>/dev/null || builtin echo \$CUTBUFFER)\"
+        CUTBUFFER=\"\$(paste widget 2>/dev/null || builtin echo \${CUTBUFFER})\"
         zle .${widget}
       }
     "
@@ -94,11 +94,11 @@ wrap_clipboard_widgets_paste() {
 }
 
 wrap_clipboard_widgets_copy \
-    vi-yank vi-yank-eol vi-yank-whole-line \
-    vi-change vi-change-eol vi-change-whole-line \
-    vi-kill-line vi-kill-eol vi-backward-kill-word \
-    vi-delete vi-backward-delete-char
+  vi-yank vi-yank-eol vi-yank-whole-line \
+  vi-change vi-change-eol vi-change-whole-line \
+  vi-kill-line vi-kill-eol vi-backward-kill-word \
+  vi-delete vi-backward-delete-char
 wrap_clipboard_widgets_paste \
-    vi-put-{before,after} \
-    put-replace-selection
+  vi-put-{before,after} \
+  put-replace-selection
 unset -f wrap_clipboard_widgets_{copy,paste}

dc92532538214fde4dfef11dde123a42bf363a92
Nico Lintner <git@nlintn.de>
2026-08-10 02:51:14 +0200

    feat: additional vicinae config changes

--- home/users/nico/wm/common/vicinae.nix
+++ home/users/nico/wm/common/vicinae.nix
@@ -1,10 +1,14 @@
 {
   config,
+  lib,
   pkgs,
   userSettings,
   ...
 }:
 
+let
+  webSearchSctId = "sct-websearch";
+in
 {
   programs.vicinae = {
     enable = true;
@@ -15,14 +19,16 @@
       bluetooth
       color-converter
       firefox
+      nerdfont-search
       nix
+      player-pilot
     ];
 
     settings = {
       close_on_focus_loss = true;
-      search_files_in_root = true;
       pop_to_root_on_close = false;
       favicon_service = "twenty";
+      search_files_in_root = true;
       font = {
         normal = {
           family = userSettings.default-font.name;
@@ -39,6 +45,7 @@
           icon_theme = config.gtk.iconTheme.name;
         };
       };
+      telemetry.system_info = false;
       launcher_window = {
         opacity = 0.75;
         client_side_decorations = {
@@ -46,6 +53,7 @@
           rounding = 10;
           border_width = 2;
         };
+        compact_mode.enabled = true;
         layer_shell = {
           enabled = true;
           scope = "vicinae";
@@ -53,24 +61,37 @@
           layer = "top";
         };
       };
+      fallbacks = [
+        "shortcuts:${webSearchSctId}"
+        "files:search"
+      ];
       providers = {
-        "@Gelei/bluetooth-0" = {
-          preferences = {
-            connectionToggleable = true;
-          };
+        "@Gelei/vicinae-extension-bluetooth-unstable" = {
+          preferences.connectionToggleable = true;
+        };
+        "@knoopx/vicinae-extension-firefox-0" = {
+          preferences.profile_dir = config.programs.firefox.configPath;
         };
         applications = {
           preferences = {
             defaultAction = "launch";
-            inherit (config.vars) launchPrefix;
+            launchPrefix = config.vars.launchPrefix;
           };
         };
         core = {
           entrypoints = {
+            forget-telemetry.enabled = true;
             inspect-local-storage.enabled = true;
             sponsor.enabled = false;
           };
         };
+        files.preferences = {
+          autoIndexing = true;
+          excludedIndexingPaths = [ config.xdg.cacheHome ];
+          indexingPaths = [ config.home.homeDirectory ];
+        };
+        system.entrypoints.browse-apps.enabled = true;
+        theme.entrypoints.set.enabled = false;
       };
     };
 
@@ -116,5 +137,19 @@
       };
     };
   };
-  # xdg.configFile."vicinae/settings.json".force = true;
+  xdg.dataFile."vicinae/shortcuts/shortcuts.json" = {
+    force = true;
+    text = lib.toJSON [
+      {
+        id = webSearchSctId;
+        name = "Search with DuckDuckGo";
+        icon = "icon://favicon/duckduckgo.com?fallback=icon://omnicast/image?fill%3Dprimary-text";
+        url = "https://duckduckgo.com/?q={argument}";
+        app = "firefox.desktop";
+        openCount = 0;
+        createdAt = 0;
+        updatedAt = 0;
+      }
+    ];
+  };
 }

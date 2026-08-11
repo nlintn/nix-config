446e06d4b1d7ea2e091cc1df1bf426546c5e8c3c
Nico Lintner <git@nlintn.de>
2026-08-11 04:20:25 +0200

    refactor: cleanup hyprland config

--- home/users/nico/wm/hyprland/hypr-settings.nix
+++ home/users/nico/wm/hyprland/hypr-settings.nix
@@ -12,22 +12,25 @@ let
 
   inherit (config.vars) launchPrefix;
 
-  brightnessctl = "${launchPrefix} ${lib.getExe' config.services.avizo.package "lightctl"} -e 2";
-  browser = "${launchPrefix} ${lib.getExe config.programs.firefox.finalPackage}";
-  filemanager = "${launchPrefix} ${lib.getExe config.programs.nautilus.package}";
-  hyprpicker = "${launchPrefix} ${lib.getExe pkgs.hyprpicker}";
-  hyprtabs = "${launchPrefix} ${lib.getExe (pkgs.callPackage ./scripts/hyprtabs.nix args)}";
-  hyreload = "${launchPrefix} ${pkgs.callPackage ./scripts/hyreload.nix args}";
-  lock-transparent = "${launchPrefix} ${lib.getExe (pkgs.callPackage ./swaylock/lock-transparent.nix args)}";
-  playerctl = "${launchPrefix} ${lib.getExe config.services.playerctld.package}";
-  pwm = "${launchPrefix} ${lib.getExe config.programs.keepassxc.package}";
-  screenshot = "${launchPrefix} ${pkgs.callPackage ./scripts/screenshot.nix args}";
-  swaync-client = "${launchPrefix} ${lib.getExe' config.services.swaync.package "swaync-client"}";
-  term_tmux_scratchpad = "${launchPrefix} ${xdg-terminal-exec} -- ${lib.getExe config.programs.sesh.package} connect \"scratchpad 󱞂 \"";
-  term_tmux_sesh = "${launchPrefix} ${xdg-terminal-exec} -- ${config.vars.seshFzf}";
-  vicinae = "${launchPrefix} ${lib.getExe config.programs.vicinae.package}";
-  volumectl = "${launchPrefix} ${lib.getExe' config.services.avizo.package "volumectl"}";
-  xdg-terminal-exec = "${launchPrefix} ${lib.getExe config.xdg.terminal-exec.package}";
+  brightnessctl = launchPrefix + "${lib.getExe' config.services.avizo.package "lightctl"} -e 2";
+  browser = launchPrefix + "${lib.getExe config.programs.firefox.finalPackage}";
+  filemanager = launchPrefix + "${lib.getExe config.programs.nautilus.package}";
+  hyprpicker = launchPrefix + "${lib.getExe pkgs.hyprpicker}";
+  hyprtabs = launchPrefix + "${lib.getExe (pkgs.callPackage ./scripts/hyprtabs.nix args)}";
+  hyreload = launchPrefix + "${pkgs.callPackage ./scripts/hyreload.nix args}";
+  lock-transparent =
+    launchPrefix + "${lib.getExe (pkgs.callPackage ./swaylock/lock-transparent.nix args)}";
+  playerctl = launchPrefix + "${lib.getExe config.services.playerctld.package}";
+  pwm = launchPrefix + "${lib.getExe config.programs.keepassxc.package}";
+  screenshot = launchPrefix + "${pkgs.callPackage ./scripts/screenshot.nix args}";
+  swaync-client = launchPrefix + "${lib.getExe' config.services.swaync.package "swaync-client"}";
+  term_tmux_scratchpad =
+    launchPrefix
+    + "${xdg-terminal-exec} -- ${lib.getExe config.programs.sesh.package} connect \"scratchpad 󱞂 \"";
+  term_tmux_sesh = launchPrefix + "${xdg-terminal-exec} -- ${config.vars.seshFzf}";
+  vicinae = launchPrefix + "${lib.getExe config.programs.vicinae.package}";
+  volumectl = launchPrefix + "${lib.getExe' config.services.avizo.package "volumectl"}";
+  xdg-terminal-exec = launchPrefix + "${lib.getExe config.xdg.terminal-exec.package}";
 
 in
 with config.colorScheme.palette;

4794783a0bb46432e846fa5043e552aa06cc365e
Nico Lintner <git@nlintn.de>
2026-09-23 03:41:52 +0200

    refactor: use flake registry uris

--- flake.nix
+++ flake.nix
@@ -90,8 +90,8 @@
     };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "nixpkgs/nixos-26.05";
 
     nixpkgs-overlay = {
       url = "github:nlintn/nixpkgs-overlay";
